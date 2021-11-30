39a572faf3ca38d7461e9d825af1963997210fb5
guswnsakvk <guswnsakvk1@naver.com>
2021-11-30 16:12:10 +0900

    save image

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const colors = document.getElementsByClassName("jsColor")
 const ctx = canvas.getContext("2d")
 const range = document.getElementById("jsRange")
 const mode = document.getElementById("jsMode")
+const saveBtn = document.getElementById("jsSave")
 
 const INITIAL_COLOR = "#2C2C2C"
 
@@ -12,6 +13,8 @@ const canvas_height = 500
 canvas.width = canvas_width
 canvas.height = canvas_height
 
+ctx.fillStyle = "white"
+ctx.fillRect(0,0,canvas_width,canvas_height)
 ctx.strokeStyle = "INITIAL_COLOR"
 ctx.fillStyle = "INITIAL_COLOR"
 ctx.lineWidth = 2.5
@@ -72,12 +75,25 @@ function canvasClick(){
   }
 }
 
+function CM(event){
+  event.preventDefault()
+}
+
+function saveClick(event){
+  const image = canvas.toDataURL()
+  const link = document.createElement("a")
+  link.href = image
+  link.download = "PaintJS"
+  link.click()
+}
+
 if(canvas){
   canvas.addEventListener("mousemove", onMouseMove)
   canvas.addEventListener("mousedown", startPainting)
   canvas.addEventListener("mouseup", stopPainting)
   canvas.addEventListener("mouseleave", stopPainting)
   canvas.addEventListener("click", canvasClick)
+  canvas.addEventListener("contextmenu", CM)
 }
 
 Array.from(colors).forEach(color => color.addEventListener("click", changeColor))
@@ -88,4 +104,8 @@ if(range){
 
 if(mode){
   mode.addEventListener("click",modeClick)
+}
+
+if(saveBtn){
+  saveBtn.addEventListener("click", saveClick)
 }
