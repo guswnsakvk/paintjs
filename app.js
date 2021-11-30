261a2bbc552c11d8bc86bddb88f0c859418615bc
guswnsakvk <guswnsakvk1@naver.com>
2021-11-30 11:51:19 +0900

    Filling Mode

--- app.js
+++ app.js
@@ -2,14 +2,22 @@ const canvas = document.getElementById("jsCanvas")
 const colors = document.getElementsByClassName("jsColor")
 const ctx = canvas.getContext("2d")
 const range = document.getElementById("jsRange")
+const mode = document.getElementById("jsMode")
 
-canvas.width = 600
-canvas.height = 500
+const INITIAL_COLOR = "#2C2C2C"
 
-ctx.strokeStyle = "#2c2c2c"
+const canvas_width = 600
+const canvas_height = 500
+
+canvas.width = canvas_width
+canvas.height = canvas_height
+
+ctx.strokeStyle = "INITIAL_COLOR"
+ctx.fillStyle = "INITIAL_COLOR"
 ctx.lineWidth = 2.5
 
 let painting = false
+let filling = false
 
 function stopPainting(){
   painting = false
@@ -38,6 +46,7 @@ function onMouseUp(event){
 function changeColor(event){
   const color = event.target.style.backgroundColor
   ctx.strokeStyle = color
+  ctx.fillStyle = ctx.strokeStyle
 }
 
 function rangeChange(event){
@@ -46,15 +55,37 @@ function rangeChange(event){
   ctx.lineWidth = size
 }
 
+function modeClick(event){
+  if(filling === true){
+    filling = false
+    mode.innerText = "Fill"
+  }
+  else{
+    filling = true
+    mode.innerText = "PAINT"
+  }
+}
+
+function canvasClick(){
+  if (filling){
+    ctx.fillRect(0,0,canvas_width,canvas_height)
+  }
+}
+
 if(canvas){
   canvas.addEventListener("mousemove", onMouseMove)
-  canvas.addEventListener("mousedown", startPainting);
-  canvas.addEventListener("mouseup", stopPainting);
+  canvas.addEventListener("mousedown", startPainting)
+  canvas.addEventListener("mouseup", stopPainting)
   canvas.addEventListener("mouseleave", stopPainting)
+  canvas.addEventListener("click", canvasClick)
 }
 
 Array.from(colors).forEach(color => color.addEventListener("click", changeColor))
 
 if(range){
   range.addEventListener("input", rangeChange)
+}
+
+if(mode){
+  mode.addEventListener("click",modeClick)
 }
